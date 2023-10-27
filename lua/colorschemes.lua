bea94b75878bd3da02fe0d8a1eeb32aff929c69b
Callahan Kovacs <callahankovacs@gmail.com>
2023-10-27 08:45:02 -0500

    simplify colorschemes
    Signed-off-by: Callahan Kovacs <callahankovacs@gmail.com>

--- lua/colorschemes.lua
+++ lua/colorschemes.lua
@@ -1,47 +1,11 @@
 --- This module will load a random colorscheme on nvim startup process.
 
-local utils = require("utils")
+-- foreground option can be material, mix, or original
+vim.g.gruvbox_material_foreground = "material"
+--background option can be hard, medium, soft
+vim.g.gruvbox_material_background = "soft"
+vim.g.gruvbox_material_enable_italic = 1
+vim.g.gruvbox_material_enable_bold = 1
+vim.g.gruvbox_material_better_performance = 1
 
-local M = {}
-
--- Colorscheme to its directory name mapping, because colorscheme repo name is not necessarily
--- the same as the colorscheme name itself.
-M.colorscheme2dir = {
-  gruvbox_material = "gruvbox-material",
-}
-
-M.gruvbox_material = function()
-  -- foreground option can be material, mix, or original
-  vim.g.gruvbox_material_foreground = "material"
-  --background option can be hard, medium, soft
-  vim.g.gruvbox_material_background = "soft"
-  vim.g.gruvbox_material_enable_italic = 1
-  vim.g.gruvbox_material_better_performance = 1
-
-  vim.cmd([[colorscheme gruvbox-material]])
-end
-
-
---- Use a random colorscheme from the pre-defined list of colorschemes.
-M.rand_colorscheme = function()
-  local colorscheme = utils.rand_element(vim.tbl_keys(M.colorscheme2dir))
-
-  if not vim.tbl_contains(vim.tbl_keys(M), colorscheme) then
-    local msg = "Invalid colorscheme: " .. colorscheme
-    vim.notify(msg, vim.log.levels.ERROR, { title = "nvim-config" })
-
-    return
-  end
-
-  -- Load the colorscheme and its settings
-  M[colorscheme]()
-
-  if vim.g.logging_level == "debug" then
-    local msg = "Colorscheme: " .. colorscheme
-
-    vim.notify(msg, vim.log.levels.DEBUG, { title = "nvim-config" })
-  end
-end
-
--- Load a random colorscheme
-M.rand_colorscheme()
+vim.cmd([[colorscheme gruvbox-material]])
